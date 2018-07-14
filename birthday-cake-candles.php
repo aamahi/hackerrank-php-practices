44bfe16e97be819088f5a60331023e109445b62a
emtiazzahid <zahid@nextdot.com.au>
2018-07-14 14:06:33 +0600

    birthday-cake-candles solved

---
+++ birthday-cake-candles.php
@@ -0,0 +1,23 @@
+<?php
+
+// Complete the birthdayCakeCandles function below.
+function birthdayCakeCandles($ar) {
+     return array_count_values($ar)[max($ar)];
+}
+
+$fptr = fopen("OUTPUT_PATH.txt", "w");
+
+$stdin = fopen("php://stdin", "r");
+
+fscanf($stdin, "%d\n", $ar_count);
+
+fscanf($stdin, "%[^\n]", $ar_temp);
+
+$ar = array_map('intval', preg_split('/ /', $ar_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = birthdayCakeCandles($ar);
+
+fwrite($fptr, $result . "\n");
+
+fclose($stdin);
+fclose($fptr);
